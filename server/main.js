171f15485eb1eb09b910fe70f965adcdbd47025e
Will Wright <will@clazzoo.com>
2017-02-13 12:39:21 +0000

    Correctly redirect if token is valid

--- server/main.js
+++ server/main.js
@@ -10,11 +10,26 @@ Meteor.startup(() => {
   });
 });
 
+// Executed on visits with a route like `localhost:3000/abcd`
+function onRoute(req, res, next) {
+  // Take the token and find first matching link in Links collection
+  const link = Links.findOne({ token: req.params.token });
+
+  if (link) {
+    // If link object found: redirect to Long URL
+    res.writeHead(307, { 'Location': link.url });
+    res.end();
+  } else {
+    // Else redirect to React App
+    next();
+  }
+}
+
 const middleware = ConnectRoute(function (router) {
   // localhost:3000/ NO MATCH
   // localhost:3000/books/abcd NO MATCH
   // localhost:3000/abcd WILL MATCH!!
-  router.get('/:token', (req) => console.log(req));
+  router.get('/:token', onRoute);
 });
 
 WebApp.connectHandlers.use(middleware);
