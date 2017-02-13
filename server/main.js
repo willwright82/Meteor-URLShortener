863c9a2c4570e6fa1e6b341185616dc11442e461
Will Wright <will@clazzoo.com>
2017-02-13 12:20:13 +0000

    Handle redirects based on token in URL using connect-route middleware

--- server/main.js
+++ server/main.js
@@ -1,5 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import { Links } from '../imports/collections/links';
+import { WebApp } from 'meteor/webapp';
+import ConnectRoute from 'connect-route';
 
 Meteor.startup(() => {
   // code to run on server at startup
@@ -7,3 +9,12 @@ Meteor.startup(() => {
     return Links.find({});
   });
 });
+
+const middleware = ConnectRoute(function (router) {
+  // localhost:3000/ NO MATCH
+  // localhost:3000/books/abcd NO MATCH
+  // localhost:3000/abcd WILL MATCH!!
+  router.get('/:token', (req) => console.log(req));
+});
+
+WebApp.connectHandlers.use(middleware);
