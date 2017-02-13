afc8f119d9ab055d792890f565e8f7571c29b332
Will Wright <will@clazzoo.com>
2017-02-13 11:04:50 +0000

    Added a 'links.insert' method for securely adding urls to Database

--- client/components/link_create.js
+++ client/components/link_create.js
@@ -4,7 +4,7 @@ class LinkCreate extends Component {
   handleSubmit(event) {
     event.preventDefault();
 
-    console.log(this.refs.input.value);
+    Meteor.call('links.insert', this.refs.link.value);
   }
 
   render() {
@@ -12,7 +12,7 @@ class LinkCreate extends Component {
       <form onSubmit={this.handleSubmit.bind(this)}>
         <div className="form-group">
           <label>Link to Shorten</label>
-          <input ref="input" className="form-control" />
+          <input ref="link" className="form-control" />
         </div>
         <button className="btn btn-primary">Shorten!</button>
       </form>
